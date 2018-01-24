4207d2bbcfc3873688189029981c3c5af7c3a0ba
Unknown <greisonsantos03@gmail.com>
2018-01-23 23:09:19 -0200

    arquivos e pagina index

---
+++ system/cadastro-login/cadastro.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> </title>
+	<link rel="stylesheet" href="css/font-awesome/css/font-awesome.min.css" />
+	
+	<link rel="stylesheet" href="../../bootstrap/css/bootstrap.min.css" />
+	<link rel="stylesheet" href="../../css/system/cadastro-login.css">
+
+</head>
+<body>
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h1 class="text-center">Bem Vindo!!!</h1>
+			</div>
+			<div class="modal-body">
+				<div class="container">
+					<form>
+						<div class="form-group row">
+
+							<div class="col-md-12">
+								<div class="col-md-12">
+									<label for="inputEmail3" class="col-sm-2 col-form-label">Nome</label>
+									<div class="col-sm-10">
+										<input type="text" class="form-control" id="nome" placeholder="Name...">
+									</div>
+								</div>
+							</div>
+
+							<br><br>
+							<div class="col-md-12">
+								<div class="col-md-12">
+									<label for="inputEmail3" class="col-sm-2 col-form-label">Cpf</label>
+									<div class="col-sm-10">
+										<input type="text" class="form-control" id="cpf" placeholder="Cpf...">
+									</div>
+								</div>
+							</div>
+
+							<br><br>
+							<div class="col-md-12">
+								<div class="col-md-12">
+									<label for="inputEmail3" class="col-sm-2 col-form-label">Endereço</label>
+									<div class="col-sm-10">
+										<input type="text" class="form-control" id="address" placeholder="Endereço...">
+									</div>
+								</div>
+							</div>
+
+							<br><br>
+							<div class="col-md-12">
+								<div class="col-md-12">
+									<label for="inputEmail3" class="col-sm-2 col-form-label">Telefone</label>
+									<div class="col-sm-10">
+										<input type="text" class="form-control" id="phone" placeholder="telefone...">
+									</div>
+								</div>
+							</div>
+
+                            <br><br>
+							<div class="col-md-12">
+								<div class="col-md-12">
+									<label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
+									<div class="col-sm-10">
+										<input type="email" class="form-control" id="email" placeholder="Email...">
+									</div>
+								</div>
+							</div>
+
+							<br><br>
+							<div class="col-md-12">
+								<div class="col-md-12">
+
+									<label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
+									<div class="col-sm-10">
+										<input type="password" class="form-control" id="Password" placeholder="Password...">
+									</div>
+
+								</div>
+							</div>
+						</div>
+					</form>
+					<div class="col-md-12">
+						<div class="col-md-12">
+							<div class="form-group">
+								<input type="submit" class="btn btn-block btn-lg btn-primary" value="Cadastrar"/>
+								<span class="pull-right"><a href="login.php">Login</a></span><span><a href="#">Esqueci minha Senha</a></span>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+	</html>
---
+++ system/cadastro-login/login.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> </title>
+	<link rel="stylesheet" href="../../css/font-awesome/css/font-awesome.min.css" />
+	
+	<link rel="stylesheet" href="../../bootstrap/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="../../css/system/cadastro-login.css">
+</head>
+</head>
+<body>
+    <div class="modal-dialog">
+    <div class="modal-content">
+        <div class="modal-header">
+
+          <h1 class="text-center">Bem Vindo...</h1>
+        </div>
+         <div class="modal-body">
+             <div class="form-group">
+                 <input type="text" class="form-control input-lg" placeholder="Username"/>
+             </div>
+
+             <div class="form-group">
+                 <input type="password" class="form-control input-lg" placeholder="Password"/>
+             </div>
+
+             <div class="form-group">
+                 <input type="submit" class="btn btn-block btn-lg btn-primary" value="Login"/>
+                 <span class="pull-right"><a href="cadastro.php">Cadastrar</a></span><span><a href="#">Esqueci Minha Senha</a></span>
+             </div>
+         </div>
+    </div>
+ </div>
+</body>
+</html>
